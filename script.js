25d673d7f3ea251086690037881ee7e93ea92712
unknown <guyrdmello@gmail.com>
2021-11-20 23:49:24 -0500

    website

--- script.js
+++ script.js
@@ -3,34 +3,113 @@ function update() {
     // var n2 = parseFloat(document.getElementById('input2').value);
     
     var calc = document.getElementById('calculators').value;
+    if (calc === 'Calculator-Choice') {
+        document.getElementById('input1').value = '';
+        document.getElementById('input2').value = '';
+        document.getElementById('input3').value = '';
+        document.getElementById('input4').value = '';
+        document.getElementById('input5').value = '';
+
+        document.getElementById('input1header').innerText = '';
+        document.getElementById('input2header').innerText = '';
+        document.getElementById('input3header').innerText = '';
+        document.getElementById('input4header').innerText = '';
+        document.getElementById('input5header').innerText = '';
+
+        document.getElementById('input1header').style.display = 'none';
+        document.getElementById('input2header').style.display = 'none';
+        document.getElementById('input3header').style.display = 'none';
+        document.getElementById('input4header').style.display = 'none';
+        document.getElementById('input5header').style.display = 'none';
+
+        document.getElementById('input1').type = 'hidden';
+        document.getElementById('input2').type = 'hidden';
+        document.getElementById('input3').type = 'hidden';
+        document.getElementById('input4').type = 'hidden';
+        document.getElementById('input5').type = 'hidden';
+
+        document.getElementById('Calculate').hidden = 'hidden';
+
+        document.getElementById('OutputBox').style.display = 'none';
+    }
     if(calc === 'Gravitational-Force-Calculator') {
-        document.getElementById('title').innerText = 'Gravitational Force Calculator'
-        
+        // document.getElementById('title').innerText = 'Gravitational Force Calculator'
+        document.getElementById('input1').value = '';
+        document.getElementById('input2').value = '';
+        document.getElementById('input3').value = '';
+        document.getElementById('input4').value = '';
+        document.getElementById('input5').value = '';
+
         document.getElementById('input1header').innerText = 'Mass 1 (m)';
         document.getElementById('input2header').innerText = 'Mass 2 (m)';
         document.getElementById('input3header').innerText = 'Distance (R)';
-        document.getElementById('input4header').innerText = 'Gravitational force (F)';
-        
-        document.getElementById('input1header').visibility = 'visible';
+        document.getElementById('input4header').innerText = '';
+        document.getElementById('input5header').innerText = '';
 
         document.getElementById('input1header').style.display = '';
         document.getElementById('input2header').style.display = '';
         document.getElementById('input3header').style.display = '';
-        document.getElementById('input4header').style.display = '';
+        document.getElementById('input4header').style.display = 'none';
         document.getElementById('input5header').style.display = 'none';
 
         document.getElementById('input1').type = 'text';
         document.getElementById('input2').type = 'text';
         document.getElementById('input3').type = 'text';
-        document.getElementById('input4').type = 'text';
+        document.getElementById('input4').type = 'hidden';
         document.getElementById('input5').type = 'hidden';
+
+        document.getElementById('Calculate').hidden = "";
+
+        document.getElementById('OutputBox').style.display = '';
+        document.getElementById('result').innerText = ''
+
+
+
     }
     if(calc === 'Force-Calculator') {
-        document.getElementById('title').innerText = 'Force Calculater';
+        // document.getElementById('title').innerText = 'Force Calculater';
+        document.getElementById('input1').value = '';
+        document.getElementById('input2').value = '';
+        document.getElementById('input3').value = '';
+        document.getElementById('input4').value = '';
+        document.getElementById('input5').value = '';
 
         document.getElementById('input1header').innerText = 'Mass (m)';
         document.getElementById('input2header').innerText = 'Acceleration (a)';
-        document.getElementById('input3header').innerText = 'Force (F)';
+        document.getElementById('input3header').innerText = '';
+        document.getElementById('input4header').innerText = '';
+        document.getElementById('input5header').innerText = '';
+        
+        document.getElementById('input1header').style.display = '';
+        document.getElementById('input2header').style.display = '';
+        document.getElementById('input3header').style.display = 'none';
+        document.getElementById('input4header').style.display = 'none';
+        document.getElementById('input5header').style.display = 'none';
+
+        document.getElementById('input1').type = 'text';
+        document.getElementById('input2').type = 'text';
+        document.getElementById('input3').type = 'hidden';
+        document.getElementById('input4').type = 'hidden';
+        document.getElementById('input5').type = 'hidden';
+
+        document.getElementById('Calculate').hidden = "";
+
+        document.getElementById('OutputBox').style.display = '';
+        document.getElementById('result').innerText = ''
+    }
+    if(calc === 'Acceleration-Calculator') {
+        // document.getElementById('title').innerText = 'Force Calculater';
+        document.getElementById('input1').value = '';
+        document.getElementById('input2').value = '';
+        document.getElementById('input3').value = '';
+        document.getElementById('input4').value = '';
+        document.getElementById('input5').value = '';
+
+        document.getElementById('input1header').innerText = 'Initial Speed (m/s)';
+        document.getElementById('input2header').innerText = 'Final Speed (m/s)';
+        document.getElementById('input3header').innerText = 'Time (s)';
+        document.getElementById('input4header').innerText = '';
+        document.getElementById('input5header').innerText = '';
         
         document.getElementById('input1header').style.display = '';
         document.getElementById('input2header').style.display = '';
@@ -43,32 +122,76 @@ function update() {
         document.getElementById('input3').type = 'text';
         document.getElementById('input4').type = 'hidden';
         document.getElementById('input5').type = 'hidden';
+
+        document.getElementById('Calculate').hidden = "";
+
+        document.getElementById('OutputBox').style.display = '';
+        document.getElementById('result').innerText = ''
+    }
+    if(calc === 'Friction-Calculator') {
+        // document.getElementById('title').innerText = 'Force Calculater';
+        document.getElementById('input1').value = '';
+        document.getElementById('input2').value = '';
+        document.getElementById('input3').value = '';
+        document.getElementById('input4').value = '';
+        document.getElementById('input5').value = '';
+
+        document.getElementById('input1header').innerText = 'Friction Coefficient (μ)';
+        document.getElementById('input2header').innerText = 'Normal Force (N)';
+        document.getElementById('input3header').innerText = '';
+        document.getElementById('input4header').innerText = '';
+        document.getElementById('input5header').innerText = '';
+        
+        document.getElementById('input1header').style.display = '';
+        document.getElementById('input2header').style.display = '';
+        document.getElementById('input3header').style.display = 'none';
+        document.getElementById('input4header').style.display = 'none';
+        document.getElementById('input5header').style.display = 'none';
+
+        document.getElementById('input1').type = 'text';
+        document.getElementById('input2').type = 'text';
+        document.getElementById('input3').type = 'hidden';
+        document.getElementById('input4').type = 'hidden';
+        document.getElementById('input5').type = 'hidden';
+
+        document.getElementById('Calculate').hidden = "";
+
+        document.getElementById('OutputBox').style.display = '';
+        document.getElementById('result').innerText = ''
     }
 }
+
 function calc()
 {
     var n1 = parseFloat(document.getElementById('input1').value);
     var n2 = parseFloat(document.getElementById('input2').value);
+    var n3 = parseFloat(document.getElementById('input3').value);
+    var n4 = parseFloat(document.getElementById('input4').value);
+    var n5 = parseFloat(document.getElementById('input5').value);
     
-    var oper = document.getElementById('operators').value;
+    var calc = document.getElementById('calculators').value;
     
-    if(oper === 'gravitational-force')
+    if(calc === 'Gravitational-Force-Calculator')
     {
-        document.getElementById('result').value = n1+n2;
+        document.getElementById('result').style.display  = '';
+        document.getElementById('result').innerText = (n1 * n2 * 6.674 * 10 **(-11)) / (n3 ** 2) + ' N';
     }
     
-    if(oper === '-')
+    if(calc === 'Force-Calculator')
     {
-        document.getElementById('result').value = n1-n2;
+        document.getElementById('result').style.display  = '';
+        document.getElementById('result').innerText = n1 * n2 + ' N';
     }
     
-    if(oper === '/')
+    if(calc === 'Acceleration-Calculator')
     {
-        document.getElementById('result').value = n1/n2;
+        document.getElementById('result').style.display  = '';
+        document.getElementById('result').innerText = (n2 - n1) / n3 + ' m/s^2';
     }
     
-    if(oper === 'X')
+    if(calc === 'Friction-Calculator')
     {
-        document.getElementById('result').value = n1*n2;
+        document.getElementById('result').style.display  = '';
+        document.getElementById('result').innerText = n1 * n2 + ' N';
     }
 }
